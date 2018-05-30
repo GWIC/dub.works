d510bd372821ed8efd29a2569f87ebb18857125e
Nam Tran <tranngocnam97@gmail.com>
2018-05-30 05:47:08 -0400

    Recursive cp

--- provision.sh
+++ provision.sh
@@ -24,6 +24,6 @@ else
 fi
 
 # Directory setup
-cp /vagrant/dub.works ./dub.works
+cp -r /vagrant/dub.works ./dub.works
 mount --bind /home/vagrant/dub.works/.meteor/ /vagrant/dub.works/.meteor/
 echo "sudo mount --bind /home/vagrant/dub.works/.meteor/ /vagrant/dub.works/.meteor/" >> ~/.bashrc && source ~/.bashrc
