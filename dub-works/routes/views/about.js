ca3c77614660104a088013ef491c1f1b1c2c82a9
Nam Tran <tranngocnam97@gmail.com>
2018-08-24 04:26:08 -0400

    Made some more changes

--- dub-works/routes/views/about.js
+++ dub-works/routes/views/about.js
@@ -10,27 +10,26 @@ exports = module.exports = function(req, res) {
 	locals.section = 'about';
 	locals.page.title = 'About dub.works';
 
-	locals.organisers = [
-		{ name: 'Andras', image: '/images/organisers/sharkie_400_round.png', twitter: 'twalve', title: 'Ambassador', profile: '/member/sharkie' },
+	locals.fellows_distinction = [
+		{ name: 'Amb. Adras Simonyi, Ph.D', image: '/images/organisers/sharkie_400_round.png', title: '', profile: '/member/sharkie' },
 	]
 
-	locals.organisers1 = [
-		{ name: 'Annamaria Konya Tannon', image: '/images/organisers/sharkie_400_round.png', title: 'Co-Director', profile: '/member/sharkie' },
-		{ name: 'Rodney Lake', image: '/images/organisers/sharkie_400_round.png', title: 'Co-Director', profile: '/member/sharkie' },
-		{ name: 'Randy Graves', image: '/images/organisers/sharkie_400_round.png', title: 'Innovation Challenge Lead', profile: '/member/sharkie' },
-		{ name: 'Erica Cusi Worthan', image: '/images/organisers/sharkie_400_round.png', title: 'Program Lead', profile: '/member/sharkie' },
-		{ name: 'Brian Wright', image: '/images/organisers/sharkie_400_round.png', title: 'Data Science Director', profile: '/member/sharkie' },
-		{ name: 'Johnny Chou', image: '/images/organisers/sharkie_400_round.png', title: 'Program Associate', profile: '/member/sharkie' },
-		{ name: 'Bojana Jankovic', image: 'images/organisers/sharkie_400_round.png', title: 'Program Associate', profile: '/member/sharkie' }
+	locals.experts = [
+		{ name: 'Annamaria Konya Tannon', image: '/images/organisers/sharkie_400_round.png', title: '', profile: '/member/sharkie' },
+		{ name: 'Randy Graves, Ph.D', image: '/images/organisers/sharkie_400_round.png', title: '', profile: '/member/sharkie' },
+		{ name: 'Erica Wortham, Ph.D', image: '/images/organisers/sharkie_400_round.png', title: '', profile: '/member/sharkie' },
 	]
-	locals.organisers2 = [
-		{ name: 'Sarah Shavin', image: '/images/organisers/sharkie_400_round.png', title: 'Creative Fellow', profile: '/member/sharkie' },
-		{ name: 'Ryan Steed', image: '/images/organisers/sharkie_400_round.png', title: 'Technology Fellow', profile: '/member/sharkie'},
-		{ name: 'Michael Ready', image: '/images/organisers/sharkie_400_round.png', title: 'Student Innovation', profile: '/member/sharkie' },
-		{ name: 'Connor West', image: '/images/organisers/sharkie_400_round.png', title: 'Accessibility Innovation', profile: '/member/sharkie' },
-		{ name: 'Konstantin Mitic', image: '/images/organisers/sharkie_400_round.png', title: 'Student Innovation', profile: '/member/sharkie' },
-		{ name: 'Sebastian Lora', image: '/images/organisers/sharkie_400_round.png', title: 'Diversity and Inclusion', profile: '/member/sharkie' },
-		{ name: 'Isabella Sardegna', image: '/images/organisers/sharkie_400_round.png', title: 'Community Relations', profile: '/member/sharkie'},
+	locals.fellows = [
+		{ name: 'Konstantin Mitic', image: '/images/organisers/sharkie_400_round.png', title: '', profile: '/member/sharkie' },
+		{ name: 'Sarah Shavin', image: '/images/organisers/sharkie_400_round.png', title: '', profile: '/member/sharkie' },
+		{ name: 'Michael Ready', image: '/images/organisers/sharkie_400_round.png', title: '', profile: '/member/sharkie' },
+		{ name: 'Isabella Sardegna', image: '/images/organisers/sharkie_400_round.png', title: '', profile: '/member/sharkie'},
+		{ name: 'Kaitlin Santiago', image: '/images/organisers/sharkie_400_round.png', title: '', profile: '/member/sharkie'},
+		{ name: 'Connor West', image: '/images/organisers/sharkie_400_round.png', title: '', profile: '/member/sharkie' },
+		{ name: 'Steven Brunetto', image: '/images/organisers/sharkie_400_round.png', title: '', profile: '/member/sharkie'},
+		{ name: 'Erin Flynn', image: '/images/organisers/sharkie_400_round.png', title: '', profile: '/member/sharkie'},
+		{ name: 'Logan Bartholomew', image: '/images/organisers/sharkie_400_round.png', title: '', profile: '/member/sharkie'},
+		{ name: 'Mimosa Giamanco', image: '/images/organisers/sharkie_400_round.png', title: '', profile: '/member/sharkie'},
 	]
 
 	view.query('organisations', Organisation.model.find().sort('name'), 'members');
